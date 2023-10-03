abaea508f982452aae9a6c56cdb2b5e460639853
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-10-03 22:22:00 +0200

    Added a general layout of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { DetailsComponent } from './components/details/details.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchbarComponent,
+    DetailsComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
